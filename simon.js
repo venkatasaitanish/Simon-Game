160b35d379491dc41c9d506a422b0931c1b6c754
Venkata Sai Tanish Jonnadula <venkatasaitanish@gmail.com>
2021-10-11 20:24:35 +0530

    Style update and Add comments

--- simon.js
+++ simon.js
@@ -6,8 +6,9 @@ let started = false;
 let level = 0;
 let score = 0;
 let highScore = 0;
-on();
+on(); // Overlay page appears when the website is reloaded
 
+// Starting the game when Enter is pressed
 $(document).keypress(function(event){
     let key = event.code;
     console.log(key);
@@ -20,6 +21,7 @@ $(document).keypress(function(event){
     off();
 });
 
+// Starting the game when Enetr button on screen is clicked
 $(".enter-btn").click(function(){
     $("#score").text("Score: " + score);
     $("#level-title").text("Level " + level);
@@ -28,6 +30,7 @@ $(".enter-btn").click(function(){
     off();
 });
 
+// Function when color button is pressed
 $(".btn").click(function(){
     if(started){
         let userChosenColour = $(this).attr("id");
@@ -39,6 +42,7 @@ $(".btn").click(function(){
     }
 });
 
+// Checking the user pattern pressed with the actual game pattern
 function checkAnswer(currentLevel){
     if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
         if(userClickedPattern.length === gamePattern.length){
@@ -60,6 +64,7 @@ function checkAnswer(currentLevel){
     }
 }
 
+// Generating a random color for next sequence
 function nextSequence() {
     userClickedPattern = [];
     level++;
@@ -70,7 +75,8 @@ function nextSequence() {
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 }
-  
+
+// Animation to know when the color button is pressed
 function animatePress(currentColor){
     $("#" + currentColor).addClass("pressed");
     setTimeout(function(){
@@ -78,15 +84,19 @@ function animatePress(currentColor){
     }, 100);
 }
 
+// Playing sound when color button is triggered
 function playSound(name){
     let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
+// Maintaining high score and set initial values for next game
 function gameOver() {
+    // If present score is greater than high score it is updated
     if(score>highScore){
         highScore = score;
     }
+
     $("#high-score").text("Highest Score: " + highScore);
     level = 0;
     score = 0;
@@ -94,10 +104,12 @@ function gameOver() {
     started = false;
 }
 
+// Overlay page display on
 function on(){
     document.getElementById("overlay").style.display = "block";
 }
-  
+
+// Overlay page display off
 function off(){
     document.getElementById("overlay").style.display = "none";
 }
